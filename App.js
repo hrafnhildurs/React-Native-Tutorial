02de58285e4f344259c466b9beae93507140f581
Hrafnhildur Skúladóttir <hrafnhildursku15@ru.is>
2017-11-05 23:39:18 +0000

    added some mock data and displayed it

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import {
   Platform,
+  Image,
   StyleSheet,
   Text,
   View
@@ -19,19 +20,21 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'https://i.imgur.com/a5MK3cug.jpg'}},
+];
+
 export default class App extends Component<{}> {
   render() {
+  	var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image 
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
       </View>
     );
   }
@@ -44,14 +47,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  thumbnail: {
+  	width: 53,
+  	height: 81,
   },
 });
