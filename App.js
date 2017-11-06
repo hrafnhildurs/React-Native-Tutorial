7383668998aeaa7b2bce5eb3a9c06afd6efc2484
Hrafnhildur Skúladóttir <hrafnhildursku15@ru.is>
2017-11-06 21:46:30 +0000

    fetching real data

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import React, { Component } from 'react';
 import {
   Platform,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View
@@ -24,9 +25,56 @@ var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'https://i.imgur.com/a5MK3cug.jpg'}},
 ];
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 export default class App extends Component<{}> {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		dataSource: new ListView.DataSource({
+  			rowHasChanged: (row1, row2) => row1 !== row2,
+  		}),
+  		loaded: false,
+  	};
+  }
+  componentDidMount() {
+  	this.fetchData();
+  }
+  fetchData() {
+  	fetch(REQUEST_URL)
+  		.then((response)  => response.json())
+  		.then((responseData) => {
+  			this.setState({
+  				dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+  				loaded: true,
+  			});
+  		})
+  		.done();
+  }	
   render() {
-  	var movie = MOCKED_MOVIES_DATA[0];
+  	if(!this.state.loaded) {
+  		return this.renderLoadingView();
+  	}
+
+  	return (
+  		<ListView
+  		  dataSource={this.state.dataSource}
+  		  renderRow={this.renderMovie}
+  		  style={styles.listView}
+  		/>
+  	);
+  }
+
+  renderLoadingView() {
+  	return (
+  		<View style={styles.container}>
+  			<Text>
+  				Loading movies...
+  			</Text>
+  		</View>
+  	);
+  }
+
+  renderMovie(movie) {
     return (
       <View style={styles.container}>
         <Image 
@@ -64,5 +112,9 @@ const styles = StyleSheet.create({
   },
   year: {
   	textAlign: 'center'
+  },
+  listView: {
+  	paddingTop: 20,
+  	backgroundColor: '#F5FCFF',
   },
 });
